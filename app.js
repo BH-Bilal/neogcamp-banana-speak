f46c8237d34ff54f10c0c4939433649ba864c56f
Bilal-9 <bilalhussain1115@gmail.com>
2022-05-01 02:35:47 -0400

    input and output processing

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var btntranslate= document.querySelector("#btn-translate")
 var txtinput =document.querySelector("#txt-input")
 var outputDiv =document.querySelector("#output")
 
-function clickHandler() {
-    outputDiv.innerText = txtinput.value + "=  hahahaha";
 
+function clickHandler() {
+  outputDiv.innerText=txtinput.value + "hahahah" ; 
 };
 btntranslate.addEventListener("click",clickHandler)
 
